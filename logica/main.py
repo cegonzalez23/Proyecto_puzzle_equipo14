0587f168b3a00378b8bbe5dc285f535dcefd03ef
cegonzalez23 <cegonzalez@uade.edu.ar>
2025-06-21 14:07:51 -0300

    hago Update main.py para integrar el recorte de imagenes

--- logica/main.py
+++ logica/main.py
@@ -1,11 +1,13 @@
-# main.py
 from configuracion import obtener_tamaño
-from matriz import matriz_aleatoria, mostrar_matriz
+from importe_imagenes import partir_imagen
+from matriz import matriz_imagen_aleatoria, mostrar_matriz
 from movimientos import mover
 
-def jugar_rompecabezas():
+def jugar_rompecabezas_con_imagen():
     tamaño = obtener_tamaño()
-    matriz = matriz_aleatoria(tamaño)
+    ruta_imagen = input("Ingrese la ruta de la imagen (por ejemplo, 'mi_imagen.png'): ")
+    piezas = partir_imagen(ruta_imagen, tamaño, tamaño)
+    matriz = matriz_imagen_aleatoria(piezas)
 
     print("\nConfiguración inicial del rompecabezas:")
     mostrar_matriz(matriz)
@@ -13,10 +15,10 @@ def jugar_rompecabezas():
     while True:
         direccion = input("Mover (w=arriba, s=abajo, a=izquierda, d=derecha, x=Salir del juego): ").strip().lower()
         if direccion == 'x':
-            print("¡Gracias por jugaaaaaar!")
+            print("¡Gracias por jugar!")
             break
         mover(matriz, direccion)
         mostrar_matriz(matriz)
 
 if __name__ == "__main__":
-    jugar_rompecabezas()
+    jugar_rompecabezas_con_imagen()
