9a4d98e5e6e7caffdb64c659ba29836d6190dc09
cegonzalez23 <cegonzalez@uade.edu.ar>
2025-06-21 14:08:28 -0300

    hago Update matriz.py para agregar la integeacion

--- logica/matriz.py
+++ logica/matriz.py
@@ -1,18 +1,19 @@
 import random
+import os
 
-def matriz_aleatoria(cant_colum_filas):
-    letras = [chr(ord('a') + i) for i in range(cant_colum_filas * cant_colum_filas - 1)]
-    letras.append('-')
-    random.shuffle(letras)
-
+def matriz_imagen_aleatoria(piezas):
+    filas = len(piezas)
+    columnas = len(piezas[0])
+    piezas_flat = [pieza for fila in piezas for pieza in fila]
+    random.shuffle(piezas_flat)
     matriz = []
-    for i in range(cant_colum_filas):
-        fila = letras[i * cant_colum_filas: (i + 1) * cant_colum_filas]
+    for i in range(filas):
+        fila = piezas_flat[i * columnas: (i + 1) * columnas]
         matriz.append(fila)
     return matriz
 
 def mostrar_matriz(matriz):
     """Muestra la matriz del rompecabezas en pantalla."""
     for fila in matriz:
-        print('|'.join(fila))
+        print('|'.join([os.path.basename(p) if p != '-' else '-' for p in fila]))
     print()
